4357b1b15e5eae7e55ab96cb3afffaa26ac0fee6
harsh-accio <rajputharsh9897@gmail.com>
2025-09-18 12:03:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 function stringChop(str, size) {
-  // your code here
+  let result = [];
+
+  size = Number(size); // convert input to number
+  if (size <= 0) throw new Error("Chunk size must be greater than 0");
+
+  if (size >= str.length) {
+    result.push(str);
+    return result; // return array
+  }
+
+  for (let i = 0; i < str.length; i += size) {
+    result.push(str.slice(i, i + size));
+  }
+
+  return result;
 }
 
 // Do not change the code below
-const str = prompt("Enter String.");
-const size = prompt("Enter Chunk Size.");
+const str = prompt("Enter String:");
+const size = prompt("Enter Chunk Size:");
 alert(stringChop(str, size));
